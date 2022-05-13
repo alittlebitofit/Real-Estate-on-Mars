aac4336fc1fc2bbd6bcc848f8c0b5ff2a1bc3f75
alittlebitofit <pam100@pm.me>
2022-05-13 16:37:03 +0530

    added images using recycler view and Glide library

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -19,7 +19,6 @@ package com.example.android.marsrealestate.overview
 
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
@@ -27,7 +26,7 @@ import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
 /**
  * This fragment shows the the status of the Mars real-estate web services transaction.
  */
-class OverviewFragment : Fragment() {
+class OverviewFragment : androidx.fragment.app.Fragment() {
 
     /**
      * Lazily initialize our [OverviewViewModel].
@@ -43,6 +42,8 @@ class OverviewFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentOverviewBinding.inflate(inflater)
+//        val binding = GridViewItemBinding.inflate(inflater)
+
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
@@ -50,7 +51,11 @@ class OverviewFragment : Fragment() {
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
 
+        binding.photosGrid.adapter = PhotoGridAdapter()
+
         setHasOptionsMenu(true)
+
+
         return binding.root
     }
 
--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -22,7 +22,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.launch
+
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -30,11 +35,15 @@ import kotlinx.coroutines.launch
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the most recent response
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the response String
-    val response: LiveData<String>
-        get() = _response
+    val status: LiveData<MarsApiStatus>
+        get() = _status
+
+    private val _marsProperties = MutableLiveData<List<MarsProperty>>()
+    val marsProperties: LiveData<List<MarsProperty>>
+        get() = _marsProperties
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -48,13 +57,17 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsRealEstateProperties() {
         viewModelScope.launch {
-
+            _status.value = MarsApiStatus.LOADING
             try {
-                val listResult = MarsApi.retrofitService.getProperties()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                _marsProperties.value = MarsApi.retrofitService.getProperties()
+                _status.value = MarsApiStatus.DONE
             } catch (e: Exception) {
-                _response.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _marsProperties.value = ArrayList()
+
             }
+
+
 
         }
     }
